8622b0b22007c7648afbe6f1e0f9956afdcbc06e
Pedro Vicente Gómez Sánchez <pedro@karumi.com>
2020-10-12 19:17:20 +0200

    Improve main view adding more pages, buttons and measuring performance

--- DeepPanelSample/Pages.swift
+++ DeepPanelSample/Pages.swift
@@ -11,6 +11,15 @@ import Foundation
 class Pages {
     static let images = [
         "sample_page_0",
-        "sample_page_1"
+        "sample_page_1",
+        "sample_page_2",
+        "sample_page_3",
+        "sample_page_4",
+        "sample_page_5",
+        "sample_page_6",
+        "sample_page_7",
+        "sample_page_8",
+        "sample_page_9",
+        "sample_page_10",
     ]
 }
