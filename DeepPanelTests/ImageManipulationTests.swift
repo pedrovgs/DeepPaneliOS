964432e40b0a5a7fed4a1d7d018fb78184c6cd89
Daniel García <fillito@gmail.com>
2020-12-03 13:47:17 +0100

    Fix typos

--- DeepPanelTests/ImageManipulationTests.swift
+++ DeepPanelTests/ImageManipulationTests.swift
@@ -13,25 +13,25 @@ class ImageManipulationTests: XCTestCase {
 
     func testVerticalImageCanBeScaledCorrectly() throws {
         let image = imageNamed("sample_page_0.jpg")!
-        let expectedScaldeImage = imageNamed("sample_page_0_scaled.jpg")!
+        let expectedScaledImage = imageNamed("sample_page_0_scaled.jpg")!
         let targetSize = CGSize(width: 224, height: 224)
         
         let scaledImage = image.scaledImage(with: targetSize)!
         
         XCTAssertEqual(scaledImage.size, targetSize)
-        XCTAssertEqual(expectedScaldeImage.size, targetSize)
-        XCTAssertEqual(scaledImage.pngData()!, expectedScaldeImage.pngData()!)
+        XCTAssertEqual(expectedScaledImage.size, targetSize)
+        XCTAssertEqual(scaledImage.pngData()!, expectedScaledImage.pngData()!)
     }
     
     func testHorizontalImageCanBeScaledCorrectly() throws {
         let image = imageNamed("sample_page_0_rotated.jpg")!
-        let expectedScaldeImage = imageNamed("sample_page_0_rotated_scaled.jpg")!
+        let expectedScaledImage = imageNamed("sample_page_0_rotated_scaled.jpg")!
         let targetSize = CGSize(width: 224, height: 224)
         
         let scaledImage = image.scaledImage(with: targetSize)!
         
         XCTAssertEqual(scaledImage.size, targetSize)
-        XCTAssertEqual(expectedScaldeImage.size, targetSize)
-        XCTAssertEqual(scaledImage.pngData()!, expectedScaldeImage.pngData()!)
+        XCTAssertEqual(expectedScaledImage.size, targetSize)
+        XCTAssertEqual(scaledImage.pngData()!, expectedScaledImage.pngData()!)
     }
 }
