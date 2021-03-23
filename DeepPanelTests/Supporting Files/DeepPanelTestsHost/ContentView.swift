bbd6e10fc7707054800b37cca7e06de8e00d5bdc
Daniel García <fillito@gmail.com>
2021-03-23 20:14:33 +0100

    Add a host app target so we can run the tests on the main project

---
+++ DeepPanelTests/Supporting Files/DeepPanelTestsHost/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  DeepPanelTestsHost
+//
+//  Created by Daniel García on 23/3/21.
+//  Copyright © 2021 Pedro Gómez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
