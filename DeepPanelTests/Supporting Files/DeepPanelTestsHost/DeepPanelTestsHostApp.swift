23bef69ffa779f51123a65544522938da91721b0
Daniel Garcia <fillito@gmail.com>
2021-03-24 17:25:57 +0100

    Add test harness in DeepPanel (#13)
    * Add a host app target so we can run the tests on the main project
    
    * Build + run tests in CI
    
    * Run tests on iOS 14.4

---
+++ DeepPanelTests/Supporting Files/DeepPanelTestsHost/DeepPanelTestsHostApp.swift
@@ -0,0 +1,18 @@
+//
+//  DeepPanelTestsHostApp.swift
+//  DeepPanelTestsHost
+//
+//  Created by Daniel García on 23/3/21.
+//  Copyright © 2021 Pedro Gómez. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct DeepPanelTestsHostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
